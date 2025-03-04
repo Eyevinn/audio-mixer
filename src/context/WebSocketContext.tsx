23faea7066382698e0747982505e2f17c1fffd42
lucasmaupin <lucas.maupin@gmail.com>
2025-03-04 11:56:08 +0100

    feat: messageTranslator

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -3,6 +3,8 @@ import { showError, showInfo } from '../utils/notifications';
 import { AudioState } from '../types/types';
 import logger from '../utils/logger';
 import toast from 'react-hot-toast';
+import { useGlobalState } from './GlobalStateContext';
+import messageTranslator from '../utils/message-translator';
 
 interface WebSocketContextType {
   wsUrl: string;
@@ -11,8 +13,6 @@ interface WebSocketContextType {
   ) => void;
   isConnected: boolean;
   connect: (address: string) => void;
-  messages: string[];
-  setMessages: React.Dispatch<React.SetStateAction<string[]>>;
   audioState?: AudioState;
   clearAudioState: () => void;
   connectionFailed?: boolean;
@@ -23,12 +23,37 @@ const WebSocketContext = createContext<WebSocketContextType | null>(null);
 export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
+  const { setStrips, setMixes, setOutputs, setErrorMessage } = useGlobalState();
   const [ws, setWs] = useState<WebSocket | null>(null);
   const [isConnected, setIsConnected] = useState(false);
-  const [messages, setMessages] = useState<string[]>([]);
   const [audioState, setAudioState] = useState<AudioState | undefined>();
   const [wsUrl, setWsUrl] = useState<string>('');
   const [connectionFailed, setConnectionFailed] = useState<boolean>(false);
+
+  function sendMessage(
+    this: WebSocket,
+    message: Record<string, unknown> | Record<string, unknown>[]
+  ) {
+    if (!this && !ws) {
+      toast.error('Websocket is not connected', {
+        duration: 3000,
+        position: 'bottom-right'
+      });
+      return;
+    }
+
+    if ((this || ws).readyState !== WebSocket.OPEN) {
+      toast.error('Websocket lost connection', {
+        duration: 3000,
+        position: 'bottom-right'
+      });
+      setIsConnected(false);
+      return;
+    }
+
+    const messageString = JSON.stringify(message);
+    (this || ws).send(messageString);
+  }
 
   const connect = (address: string) => {
     try {
@@ -51,7 +76,14 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
             logger.data(jsonData.type, jsonData.resource, jsonData.body);
             setAudioState(jsonData);
           } else {
-            setMessages((prev) => [...prev, event.data]);
+            messageTranslator(
+              event.data,
+              sendMessage.bind(websocket),
+              setStrips,
+              setMixes,
+              setOutputs,
+              setErrorMessage
+            );
           }
         } catch (error) {
           showError('Failed to parse WebSocket message');
@@ -76,35 +108,10 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
         setIsConnected(false);
         setWsUrl('');
       };
-
       setWs(websocket);
     } catch (error) {
       showError(`Failed to create WebSocket connection: ${error}`);
     }
-  };
-
-  const sendMessage = (
-    message: Record<string, unknown> | Record<string, unknown>[]
-  ) => {
-    if (!ws) {
-      toast.error('Websocket is not connected', {
-        duration: 3000,
-        position: 'bottom-right'
-      });
-      return;
-    }
-
-    if (ws.readyState !== WebSocket.OPEN) {
-      toast.error('Websocket lost connection', {
-        duration: 3000,
-        position: 'bottom-right'
-      });
-      setIsConnected(false);
-      return;
-    }
-
-    const messageString = JSON.stringify(message);
-    ws.send(messageString);
   };
 
   const clearAudioState = () => {
@@ -118,8 +125,6 @@ export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
         sendMessage,
         isConnected,
         connect,
-        messages,
-        setMessages,
         audioState,
         clearAudioState,
         connectionFailed
---
+++ src/utils/message-translator.ts
@@ -0,0 +1,168 @@
+import { Output, TAudioStrip, TBaseStrip, TMixStrip } from '../types/types';
+import deepMerge from './deep-merge';
+import logger from './logger';
+import { getAllMixes, getAllOutputs, getAllStrips, resync } from './wsCommands';
+
+function mapData<T extends TBaseStrip>(
+  data: Record<string, T>,
+  existingStrips: T[]
+): T[] {
+  return Object.entries(data).map(([index, stripData]) => {
+    const strip = stripData as T;
+    const existingStrip = existingStrips.find(
+      (s) => s.stripId === parseInt(index)
+    );
+
+    return {
+      ...existingStrip,
+      ...strip,
+      // TODO: Redo the stripId to be a uniquestring
+      // stripId: 'Strip#' + index,
+      stripId: parseInt(index),
+      label: index === '1000' ? 'PFL' : (strip.label ?? existingStrip?.label)
+    };
+  });
+}
+
+function updateData<T extends TBaseStrip>(
+  prevStrips: T[],
+  oldStrips: T[]
+): T[] {
+  return prevStrips.map((strip) => {
+    const newStrip = oldStrips[strip.stripId];
+    if (newStrip) {
+      return {
+        ...strip,
+        ...Object.keys(newStrip).reduce((acc, key) => {
+          const typedKey = key as keyof T;
+          const updatedValue = newStrip[typedKey];
+          const currentValue = strip[typedKey];
+          if (
+            typeof updatedValue === 'object' &&
+            updatedValue !== null &&
+            currentValue &&
+            typeof currentValue === 'object'
+          ) {
+            return {
+              ...acc,
+              [typedKey]: deepMerge(currentValue, updatedValue)
+            };
+          }
+          return {
+            ...acc,
+            [typedKey]: updatedValue
+          };
+        }, {} as Partial<T>)
+      } satisfies T;
+    }
+    return strip;
+  });
+}
+
+const messageTranslator = (
+  message: string,
+  sendMessage: (
+    message: Record<string, unknown> | Record<string, unknown>[]
+  ) => void,
+  setStrips: React.Dispatch<React.SetStateAction<TAudioStrip[]>>,
+  setMixes: React.Dispatch<React.SetStateAction<TMixStrip[]>>,
+  setOutputs: React.Dispatch<React.SetStateAction<{ [key: string]: Output }>>,
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>
+) => {
+  if (!message || !sendMessage || !setStrips) return;
+  try {
+    const data = JSON.parse(message);
+    logger.data(
+      data.type,
+      data.resource || data.event,
+      data.body || data.result || data.address || 'No message'
+    );
+    switch (data.type) {
+      case 'get-response':
+        if (data.resource === '/audio/strips') {
+          setStrips((prevStrips) => mapData(data.body, prevStrips));
+        }
+        if (data.resource === '/audio/mixes') {
+          setMixes((prevMixes) => mapData(data.body, prevMixes));
+        }
+        if (data.resource === '/audio/outputs') {
+          setOutputs(data.body);
+        }
+        break;
+
+      case 'state-change':
+        if (data.body?.strips) {
+          setStrips((prevStrips: TAudioStrip[]) =>
+            updateData(prevStrips, data.body?.strips)
+          );
+        }
+        if (data.body?.mixes) {
+          setMixes((prevMixes: TMixStrip[]) =>
+            updateData(prevMixes, data.body?.mixes)
+          );
+        }
+        break;
+
+      case 'subscribe-response':
+        if (data.body?.strips) {
+          setStrips((prevStrips) => mapData(data.body.strips, prevStrips));
+        }
+        if (data.body?.mixes) {
+          setMixes((prevMixes) => mapData(data.body.mixes, prevMixes));
+        }
+        if (data.body?.outputs) {
+          setOutputs(data.body.outputs);
+        }
+        break;
+
+      case 'event':
+        if (data.event === 'connect') {
+          resync(sendMessage);
+        }
+        break;
+
+      case 'state-add':
+        if (
+          data.body.resource.startsWith('/audio/mixes/1000/inputs/mixes/') ||
+          data.body.resource.startsWith('/audio/mixes/1000/inputs/strips/')
+        ) {
+          sendMessage({
+            type: 'set',
+            resource: data.body.resource,
+            body: {
+              muted: true,
+              origin: 'pre_fader'
+            }
+          });
+        }
+        getAllStrips(sendMessage);
+        getAllMixes(sendMessage);
+        break;
+
+      case 'state-remove':
+        if (data.body.resource.startsWith('/strips/')) {
+          getAllStrips(sendMessage);
+        }
+        if (data.body.resource.startsWith('/mixes/')) {
+          getAllMixes(sendMessage);
+        }
+        break;
+      case 'command-response':
+        if (!data.body && data.error) {
+          setErrorMessage(data.error);
+        }
+        break;
+      case 'set-response':
+        if (data.resource.includes('/audio/outputs')) {
+          getAllOutputs(sendMessage);
+        }
+        if (!data.body && data.error) {
+          setErrorMessage(data.error);
+        }
+    }
+  } catch (error) {
+    console.error('Error processing WebSocket message:', error);
+  }
+};
+
+export default messageTranslator;
