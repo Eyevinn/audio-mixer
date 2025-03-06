46d34a15b4cb14667ea64a2126dc1b3d836e99d5
Saelmala <sandra.larsson@eyevinn.se>
2025-03-06 11:55:54 +0100

    fix: linting error

--- src/pages/outputs/OutputMapping.tsx
+++ src/pages/outputs/OutputMapping.tsx
@@ -5,25 +5,15 @@ import { PageHeader } from '../../components/pageLayout/pageHeader/PageHeader';
 import { ScrollableContainer } from '../../components/scrollableContainer/ScrollableContainer';
 import { EffectsPanel } from '../../components/strips/stripComponents/audioFilters/EffectsPanel';
 import { useGlobalState } from '../../context/GlobalStateContext';
-import { TAudioStrip, TMixStrip } from '../../types/types';
 
 export const OutputMappingPage = () => {
   const [selectedStrip, setSelectedStrip] = useState<{
     id: number;
     type: 'mixes' | 'strips';
   } | null>(null);
-  const [allInputs, setAllInputs] = useState<(TAudioStrip | TMixStrip)[]>([]);
   const [isFirstMount, setIsFirstMount] = useState<boolean>(true);
   const { strips, mixes, outputs, setStrips, setMixes } = useGlobalState();
-  const savedMixesWithoutPFL = useMemo(
-    () => mixes.filter((mix) => mix.stripId !== 1000),
-    [mixes]
-  );
   const isPFL = useMemo(() => mixes?.find((m) => m.stripId === 1000), [mixes]);
-
-  useEffect(() => {
-    setAllInputs([...strips, ...savedMixesWithoutPFL]);
-  }, [strips, savedMixesWithoutPFL]);
 
   useEffect(() => {
     if (!isFirstMount) return;
