10f44d2c619d6f315b64ea3b8c602f9c889455b8
Linda Malm <lindamalm@Lindas-MacBook-Pro.local>
2025-03-07 13:56:53 +0100

    fix: added clearing of ref-strips and ref-mixes plus minor fixes

--- src/components/scrollableContainer/ScrollableContainer.tsx
+++ src/components/scrollableContainer/ScrollableContainer.tsx
@@ -156,28 +156,30 @@ export const ScrollableContainer: React.FC<ScrollableContainerProps> = ({
       onMouseLeave={handleMouseUp}
       onWheel={handleWheel}
     >
-      {audioStrips?.map((strip) => (
-        <div
-          key={`${strip.stripId}-strip`}
-          ref={(el) => {
-            stripRefs.current[strip.stripId] = el;
-          }}
-        >
-          <AudioStrip
+      {audioStrips?.map((strip) => {
+        return (
+          <div
             key={`${strip.stripId}-strip`}
-            {...strip}
-            isPFLInactive={
-              isPFL?.inputs?.strips[strip.stripId]?.muted ?? undefined
-            }
-            onStripSelect={onStripSelect}
-            onRemove={() =>
-              handleRemoveStrip
-                ? handleRemoveStrip(strip.stripId)
-                : () => console.warn('No remove function provided')
-            }
-          />
-        </div>
-      ))}
+            ref={(el) => {
+              stripRefs.current[strip.stripId] = el;
+            }}
+          >
+            <AudioStrip
+              key={`${strip.stripId}-strip`}
+              {...strip}
+              isPFLInactive={
+                isPFL?.inputs?.strips?.[strip.stripId]?.muted ?? undefined
+              }
+              onStripSelect={onStripSelect}
+              onRemove={() =>
+                handleRemoveStrip
+                  ? handleRemoveStrip(strip.stripId)
+                  : () => console.warn('No remove function provided')
+              }
+            />
+          </div>
+        );
+      })}
       {mixStrips?.map((mix) => {
         if (mix.stripId === 1000) return null;
         return (
@@ -192,7 +194,7 @@ export const ScrollableContainer: React.FC<ScrollableContainerProps> = ({
               setHighlightedMixId={setHighlightedMixId}
               {...mix}
               isPFLInactive={
-                isPFL?.inputs?.mixes[mix.stripId]?.muted ?? undefined
+                isPFL?.inputs?.mixes?.[mix.stripId]?.muted ?? undefined
               }
               onStripSelect={onStripSelect}
               onRemove={() =>
@@ -221,7 +223,8 @@ export const ScrollableContainer: React.FC<ScrollableContainerProps> = ({
                   sendLevels={strip}
                   type="strips"
                   isPFLInactive={
-                    isPFL?.inputs?.strips[parseInt(key, 10)]?.muted ?? undefined
+                    isPFL?.inputs?.strips?.[parseInt(key, 10)]?.muted ??
+                    undefined
                   }
                   onStripSelect={onStripSelect}
                   onRemove={() =>
@@ -255,7 +258,8 @@ export const ScrollableContainer: React.FC<ScrollableContainerProps> = ({
                   sendLevels={mix}
                   type="mixes"
                   isPFLInactive={
-                    isPFL?.inputs?.mixes[parseInt(key, 10)]?.muted ?? undefined
+                    isPFL?.inputs?.mixes?.[parseInt(key, 10)]?.muted ??
+                    undefined
                   }
                   onStripSelect={onStripSelect}
                   onRemove={() =>
--- src/hooks/usePFLMixManager.tsx
+++ src/hooks/usePFLMixManager.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useGlobalState } from '../context/GlobalStateContext';
 import { useWebSocket } from '../context/WebSocketContext';
 import {
@@ -16,49 +16,83 @@ export const usePFLMixManager = () => {
   const processedMixes = useRef<Set<number>>(new Set());
   const pflMix = mixes.find((mix) => mix.stripId === 1000);
   const noStrips = strips.length === 0;
+  const [stripsLength, setStripsLength] = useState<number | null>(null);
+  const [mixesLength, setMixesLength] = useState<number | null>(null);
 
   const resetProcessedSets = useCallback(() => {
     processedStrips.current.clear();
     processedMixes.current.clear();
+    setStripsLength(null);
+    setMixesLength(null);
   }, []);
 
-  const setPFLMix = useCallback(() => {
+  const createPFLMix = useCallback(() => {
     resetProcessedSets();
     addMix(sendMessage, 1000);
     addInputToOutput(sendMessage, 'pfl', 1000, 'pre_fader', 'mix');
   }, [sendMessage, resetProcessedSets]);
 
+  useEffect(() => {
+    if (stripsLength !== processedStrips.current.size) {
+      processedStrips.current.clear();
+    }
+  }, [stripsLength]);
+
+  useEffect(() => {
+    if (mixesLength !== processedMixes.current.size) {
+      processedMixes.current.clear();
+    }
+  }, [mixesLength]);
+
   // Ensure PFL mix exists
   useEffect(() => {
     if (!pflMix && !noStrips) {
-      setPFLMix();
+      createPFLMix();
     }
-  }, [pflMix, noStrips, setPFLMix]);
+  }, [pflMix, noStrips, createPFLMix]);
+
+  const addStripToPFLMix = useCallback(
+    (isAlreadyInPFL: boolean, id: number) => {
+      if (!isAlreadyInPFL) {
+        addStripToMix(sendMessage, 1000, id);
+      }
+    },
+    [sendMessage]
+  );
 
   // Add new strips to PFL mix
   useEffect(() => {
-    if (!pflMix) return;
+    if (!pflMix || stripsLength === strips.length) return;
 
     strips.forEach((strip) => {
+      setStripsLength(strips.length);
       if (processedStrips.current.has(strip.stripId)) return;
 
       const isAlreadyInPFL =
         pflMix.inputs?.strips &&
         Object.keys(pflMix.inputs.strips).map(Number).includes(strip.stripId);
 
-      if (!isAlreadyInPFL) {
-        addStripToMix(sendMessage, 1000, strip.stripId);
-      }
+      addStripToPFLMix(isAlreadyInPFL, strip.stripId);
 
       processedStrips.current.add(strip.stripId);
     });
-  }, [strips, pflMix, sendMessage]);
+  }, [stripsLength, strips, pflMix, addStripToPFLMix]);
+
+  const addMixToPFLMix = useCallback(
+    (isAlreadyInPFL: boolean, id: number) => {
+      if (!isAlreadyInPFL) {
+        addMixToMix(sendMessage, 1000, id);
+      }
+    },
+    [sendMessage]
+  );
 
   // Add new mixes to PFL mix
   useEffect(() => {
-    if (!pflMix) return;
+    if (!pflMix || mixesLength === mixes.length) return;
 
     mixes.forEach((mix) => {
+      setMixesLength(mixes.length);
       if (mix.stripId === 1000 || processedMixes.current.has(mix.stripId))
         return;
 
@@ -66,11 +100,9 @@ export const usePFLMixManager = () => {
         pflMix.inputs?.mixes &&
         Object.keys(pflMix.inputs.mixes).map(Number).includes(mix.stripId);
 
-      if (!isAlreadyInPFL) {
-        addMixToMix(sendMessage, 1000, mix.stripId);
-      }
+      addMixToPFLMix(isAlreadyInPFL, mix.stripId);
 
       processedMixes.current.add(mix.stripId);
     });
-  }, [mixes, pflMix, sendMessage]);
+  }, [mixesLength, mixes, pflMix, addMixToPFLMix]);
 };
